1daadddf5121877ce8ca69431ed7b152e9ca12a2
JeffreyEssien <adizastone89@gmail.com>
2025-08-21 16:36:53 +0100

    fixation9

--- src/app/vendors/page.tsx
+++ src/app/vendors/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+
+export const dynamic = "force-dynamic";
+
 import { useState } from "react";
 import PendingOrders from "./pendingOrders";
 import ManageProducts from "./manageProducts";
